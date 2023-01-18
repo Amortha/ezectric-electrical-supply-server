26d0ee5a9ae6789e96604eb3c68812fee1e4c916
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-01-18 21:21:21 +0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors');
-require ('dotenv').config();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const e = require('express');
 
 
 const app = express()
@@ -15,44 +17,64 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-try{
+async function run() {
+  try {
     await client.connect();
-   const shopsCollection = client.db('ezectric_electrical').collection('shops');
-   const bookingCollection = client.db('ezectric_electrical').collection('bookings');
+    const shopsCollection = client.db('ezectric_electrical').collection('shops');
+    const bookingCollection = client.db('ezectric_electrical').collection('bookings');
+    const userCollection = client.db('ezectric_electrical').collection('users');
 
-   //product data api
-   app.get('/shops',async(req,res) =>{
-    const query ={};
-    const cursor = shopsCollection.find(query);
-    const  shops = await cursor.toArray();
-    res.send(shops);
-   })
-  //product data end
+    //product data api
+    app.get('/shops', async (req, res) => {
+      const query = {};
+      const cursor = shopsCollection.find(query);
+      const shops = await cursor.toArray();
+      res.send(shops);
+    })
+    //product data end
+    //login user Email get
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ result, token });
+    })
 
-// order get dashboard
-
-app.get('/booking',async(req,res) =>{
-const customerEmail= req.query.customerEmail;
-const query = {customerEmail : customerEmail};
-const bookings =await bookingCollection.find(query).toArray();
-res.send(bookings)
-})
-
-// order get dashboard end
-
-//bookings orders
-app.post('/booking',async(req,res) =>{
-  const booking = req.body;
-  const result = await bookingCollection.insertOne(booking);
-  res.send(result);
-})
+    // login user Email get end
 
 
-}
-finally{
+    // order get dashboard
 
-}
+    app.get('/booking', async (req, res) => {
+      const customerEmail = req.query.customerEmail;
+      const authorization = req.headers.authorization
+      console.log('auth header', authorization)
+      const query = { customerEmail: customerEmail };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings)
+    })
+
+    // order get dashboard end
+
+    //bookings orders
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
+
+    //booking order end
+
+  }
+  finally {
+
+  }
 }
 
 run().catch(console.dir)
