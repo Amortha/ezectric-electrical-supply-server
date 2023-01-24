0328dc81826d693126b58fdb5a2d5f24617e4216
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-01-24 22:42:43 +0600

    commit

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json())
 
-
+//mongodb pass and user
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0uwqii.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//JWT token start
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
@@ -36,6 +37,7 @@ function verifyJWT(req, res, next) {
 async function run() {
   try {
     await client.connect();
+    //browser collection name 
     const shopsCollection = client.db('ezectric_electrical').collection('shops');
     const bookingCollection = client.db('ezectric_electrical').collection('bookings');
     const userCollection = client.db('ezectric_electrical').collection('users');
@@ -48,6 +50,30 @@ async function run() {
       res.send(shops);
     })
     //product data end
+
+// All UserEmail start
+app.get('/user', verifyJWT, async(req,res) => {
+  const users = await userCollection.find().toArray();
+  res.send(users);
+})
+
+// All UserEmail end
+
+
+//Make a Admin start
+
+app.put('/user/admin/:email',verifyJWT, async (req, res) => {
+  const email = req.params.email;
+  const filter = { email: email };
+  const updateDoc = {
+    $set:{role:'admin'} ,
+  };
+  const result = await userCollection.updateOne(filter, updateDoc);
+  
+  res.send(result );
+})
+
+//Make a Admin end
     //login user Email get
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
