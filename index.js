5df9f341e3b8ae635373e7b0c1cb94830009764d
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-01-24 18:02:50 +0600

    jwt

--- index.js
+++ index.js
@@ -17,6 +17,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
   try {
     await client.connect();
@@ -51,14 +67,28 @@ async function run() {
 
     // order get dashboard
 
-    app.get('/booking', async (req, res) => {
+    app.get('/booking',verifyJWT, async (req, res) => {
       const customerEmail = req.query.customerEmail;
-      const authorization = req.headers.authorization
-      console.log('auth header', authorization)
+     const decodedEmaill =req.query.customerEmail;
+     if(customerEmail === decodedEmaill){
       const query = { customerEmail: customerEmail };
       const bookings = await bookingCollection.find(query).toArray();
-      res.send(bookings)
+      return res.send(bookings);
+     }
+      else{
+        return res.status(403).send({message:'forbidden access'});
+
+      }
     })
+
+    // app.get('/booking',verifyJWT, async (req, res) => {
+    //   const customerEmail = req.query.customerEmail
+    //   //  const authorization = req.headers.authorization
+    //   const query = {customerEmail: customerEmail};
+    //   const bookings = await bookingCollection.find(query).toArray();
+    //   res.send(bookings)
+
+    // })
 
     // order get dashboard end
 
